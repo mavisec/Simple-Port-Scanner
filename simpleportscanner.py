ff00ce35337ba660557dfcda155b97cbef2f492a
Rishab Nayyar <82491413+rishabhacking@users.noreply.github.com>
2021-05-14 00:22:21 +0530

    Update simpleportscanner.py

--- simpleportscanner.py
+++ simpleportscanner.py
@@ -4,8 +4,8 @@ import socket
 
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host= "127.0.0.1" #Enter the HOST
-port= 1234		#Enter the PORT
+host = "127.0.0.1" #Enter the HOST
+port = 1234		#Enter the PORT
 
 def port_scanner(port):
 	if sock.connect_ex((host,port)):
